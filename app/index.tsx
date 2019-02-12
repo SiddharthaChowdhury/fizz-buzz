ef157eea7e0585c96e4c52dfa509c2fc6707b35f
Siddhartha <siddharthac6@gmail.com>
2019-02-13 00:21:57 +0530

    Initialization of project

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+// import {Dashboard} from "../features/dashboard/dashboard";
+// import {ProtectedRoute} from "../features/authentication/ProtectedRoute";
+// import {Landing} from "../features/landing/Landing";
+
+const App = (): JSX.Element => (
+    <div>
+        Hello World!
+    </div>
+)
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
